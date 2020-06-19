0541bf29726c9ea763d6a1560cc1dc37b8dc1557
LiuYutian <ytliu18@lzu.edu.cn>
2020-06-19 23:17:05 +0800

    modified the time extractor class to implement time-diff extraction

--- data_extraction.py
+++ data_extraction.py
@@ -6,6 +6,8 @@ Implement data-extraction classes:
 FixDistanceExtractor: 
 get bug-fix distance of subsystems in repo linux-next;
 
+FeatureAddExtractor:
+get the development speed of features;
 """
 __author__ = "Liu Yutian"
 __copyright__ = "Copyright 2020, Lanzhou University"
@@ -34,8 +36,7 @@ class FixDistanceExtractor:
 
     def get_fix(self):
         """
-        Returns the list of bug-fix commits and the list of the
-        corresponding commits.
+        Returns the list of bug-fix commits.
         """
         fix_list = []
         cmt_list = []
@@ -58,6 +59,51 @@ class FixDistanceExtractor:
         return fix_list, cmt_list
 
 
+class FeatureAddExtractor:
+    def __init__(self, subsys, range):
+        self.subsys = subsys
+        self.range = range
+
+    def get_feature(self):
+        cmt_list = []
+
+        cmd = ["git", "log", "--oneline", self.range]
+        p = Popen(cmd, cwd=self.subsys, stdout=PIPE)
+        data, res = p.communicate()
+        # Clean and normalize the data.
+        data = unicodedata.normalize(u"NFKD", data.decode(encoding="utf-8", errors="ignore"))
+
+        for line in data.split("\n"):
+            if (re.findall("feature", str(line))):
+                cmt_list.append(line[0:11])
+
+        return cmt_list
+
+    def get_time(self, cmt_list):
+        time_list = []
+        diff_list = []
+
+        for cmt in cmt_list:
+            cmd = ["git", "log", "-1", '--pretty=format:\"%ct\"', cmt]
+            p = Popen(cmd, cwd=self.subsys, stdout=PIPE)
+            data, res = p.communicate()
+            data = unicodedata.normalize(u"NFKD", data.decode(encoding="utf-8", errors="ignore"))
+            time_list.append(int(data.strip('"')))
+
+        time_list.sort()
+        if len(time_list) == 1:
+            return None
+        if len(time_list) == 2:
+            diff_list.append(time_list[1] - time_list[0])
+        else:
+            for i in range(len(time_list) - 1):
+                diff_list.append(time_list[i + 1] - time_list[i])
+
+        return diff_list
+
+
 if __name__ == "__main__":
-    extractor = FixDistanceExtractor("/home/ytliu/linux-next/fs", "v4.4")
-    fl, cl = extractor.get_fix()
+    extractor = FeatureAddExtractor("/home/ytliu/linux-next/fs", "v4.4")
+    cmt_list = extractor.get_feature()
+    diff_list = extractor.get_time(cmt_list)
+    print(diff_list)
