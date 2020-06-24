14b3736d4c71239784448700ce95b83a4a288c30
Liu Yutian <ytliu18@lzu.edu.cn>
2020-06-24 17:40:38 +0800

    script to explore some data

---
+++ eda.py
@@ -0,0 +1,25 @@
+import data_extraction
+import numpy as np
+
+
+repo = "/home/ytliu/linux-next/"
+subsys = ["fs/afs/", "drivers/video/", "kernel/printk/", "net/nfc/", "tools/usb/"]
+range = "v4.4"
+file_static = {}
+feature_static = {}
+
+
+for sname in subsys:
+    extractor = data_extraction.FeatureAddExtractor(repo, sname, range)
+    cl = extractor.get_feature()
+    dl = extractor.get_time(cl)
+    feature_static[sname] = np.mean(dl)
+
+    extractor = data_extraction.FileAddExtractor(sname)
+    fl = extractor.get_file()
+    td = extractor.get_timediff(fl)
+    file_static[sname] = np.mean(td)
+
+
+print(feature_static)
+print(file_static)
