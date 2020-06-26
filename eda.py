c92ead6f17dc7e5a48847f1255c09e1093e43f6b
LiuYutian <ytliu18@lzu.edu.cn>
2020-06-26 13:48:36 +0800

    get the time diff list

--- eda.py
+++ eda.py
@@ -5,21 +5,22 @@ import numpy as np
 repo = "/home/ytliu/linux-next/"
 subsys = ["fs/afs/", "drivers/video/", "kernel/printk/", "net/nfc/", "tools/usb/"]
 range = "v4.4"
-file_static = {}
-feature_static = {}
+# file_static = {}
+# feature_static = {}
 
 
 for sname in subsys:
-    extractor = data_extraction.FeatureAddExtractor(repo, sname, range)
-    cl = extractor.get_feature()
-    dl = extractor.get_time(cl)
-    feature_static[sname] = np.std(dl)
+    # extractor = data_extraction.FeatureAddExtractor(repo, sname, range)
+    # cl = extractor.get_feature()
+    # dl = extractor.get_time(cl)
+    # feature_static[sname] = np.std(dl)
 
     extractor = data_extraction.FileAddExtractor(repo + sname)
     fl = extractor.get_file()
     td = extractor.get_timediff(fl)
-    file_static[sname] = np.std(td)
+    # file_static[sname] = np.std(td)
+    print(td)
 
 
-print(feature_static)
-print(file_static)
+# print(feature_static)
+# print(file_static)
